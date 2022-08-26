b444983b913942cf6c7341a1ba993f11f19aa0e4
Luis Victoria <x@lambda.lv>
2022-08-26 03:11:03 -0700

    Change hardcoded error to http.Error and use net/http constants

--- main.go
+++ main.go
@@ -21,9 +21,8 @@ func textView(w http.ResponseWriter, r *http.Request) {
 
 func textCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
