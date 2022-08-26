e5fdc5b1a179284b18e33e5f76ff6db3daa7b54f
Luis Victoria <x@lambda.lv>
2022-08-26 03:01:06 -0700

    Add textView and textCreate handlers

--- main.go
+++ main.go
@@ -5,14 +5,26 @@ import (
 	"net/http"
 )
 
-// Home handler
+// --- HANDLERS ---
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from text.tk"))
 }
 
+func textView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific text..."))
+}
+
+func textCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new text..."))
+}
+
+// --- END HANDLERS ---
+
 func main() {
 	mux := http.NewServeMux() // initialize new servemux
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/t/view", textView)
+	mux.HandleFunc("/t/create", textCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux) // start a new web server
